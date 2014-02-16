ec190942c7e803069c18b0cdd02b69acca4789fd
wddabc <wddabc@gmail.com>
2014-02-15 21:21:05 -0500

    Sat Feb 15 21:21:05 EST 2014

---
+++ 1.1/sol1.cpp
@@ -0,0 +1,12 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name : sol1.cpp
+* Purpose :
+* Creation Date : 15-02-2014
+* Last Modified : Sat Feb 15 21:21:00 2014
+* Created By : wdd 
+_._._._._._._._._._._._._._._._._._._._._.*/
+#include "general.h"
+int main(int argc, const char* argv[])
+{
+	return 0;
+}
